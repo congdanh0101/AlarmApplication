a05e4e738e2f9578d0b274041fde964460d3f4c5
congdanh0101 <congdanh010101>
2022-04-28 18:46:53 +0700

    update StopwatchFragment

--- app/src/main/java/com/example/alarm/Stopwatch/StopwatchFragment.java
+++ app/src/main/java/com/example/alarm/Stopwatch/StopwatchFragment.java
@@ -1,26 +1,153 @@
 package com.example.alarm.Stopwatch;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.alarm.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StopwatchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class StopwatchFragment extends Fragment {
+    TextView textView;
+
+    Button start, pause, reset, lap, exit;
+
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
+
+    Handler handler;
+
+    int Seconds, Minutes, MilliSeconds;
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime/10 % 100);
+
+            textView.setText(String.format("%02d",Minutes) + ":" + String.format("%02d", Seconds) + ":" + String.format("%02d", MilliSeconds));
+            handler.postDelayed(this, 0);
+        }
+
+    };
+    ListView listView;
+    String[] ListElements = new String[]{};
+    List<String> ListElementsArrayList;
+    ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = (TextView) view.findViewById(R.id.TextView);
+        start = (Button) view.findViewById(R.id.start);
+        pause = (Button) view.findViewById(R.id.pause);
+        reset = (Button) view.findViewById(R.id.reset);
+        lap = (Button) view.findViewById(R.id.lap);
+        listView = (ListView) view.findViewById(R.id.listview);
+
+        handler = new Handler();
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(adapter);
+        lap.setEnabled(false);
+        reset.setEnabled(false);
+        pause.setEnabled(false);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+                start.setEnabled(false);
+                pause.setEnabled(true);
+                reset.setEnabled(false);
+                lap.setEnabled(true);
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+                pause.setEnabled(false);
+                reset.setEnabled(true);
+                start.setEnabled(true);
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L;
+                StartTime = 0L;
+                TimeBuff = 0L;
+                UpdateTime = 0L;
+                Seconds = 0;
+                Minutes = 0;
+                MilliSeconds = 0;
+
+                textView.setText("00:00:00");
+
+                ListElementsArrayList.clear();
+                start.setEnabled(true);
+                reset.setEnabled(false);
+                lap.setEnabled(false);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ListElementsArrayList.add(textView.getText().toString());
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
     }
 }
